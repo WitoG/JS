5518ee7acf39d740361d0b73f0576e618367193c
WitoG <wit1godlewski@gmail.com>
2018-10-23 22:53:04 +0200

    JS12

---
+++ test.js
@@ -0,0 +1,17 @@
+var youLikeMeat = true;
+var myNum =10;
+
+if (youLikeMeat){
+
+    document.write("Here is the meaty menu...");
+
+}
+
+if (myNum > 11){
+
+    document.write("myNum is greater than 10");
+
+} else{
+
+    document.write("myNum is not greater than 10");
+}
